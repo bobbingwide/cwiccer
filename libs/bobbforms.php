379640ee8c9a5b69a8180dd776c75323b27899fd
Herb Miller <herb@bobbingwide.com>
2022-03-05 21:21:23 +0000

    Update shared library versions

--- libs/bobbforms.php
+++ libs/bobbforms.php
@@ -1,6 +1,6 @@
-<?php // (C) Copyright Bobbing Wide 2009-2021
+<?php // (C) Copyright Bobbing Wide 2009-2022
 if ( !defined( "BOBBFORMS_INCLUDED" ) ) {
-define( "BOBBFORMS_INCLUDED", "3.2.9" );
+define( "BOBBFORMS_INCLUDED", "3.3.0" );
 
 /**
  * Library: bobbforms
